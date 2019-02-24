bafb4572cef486969399cc8091e65ee55da09804
Aldren Terante <aldren.terante@gmail>
2019-02-24 17:45:49 +0800

    first commit

---
+++ src/utils/siteConfig.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteTitle: 'thecodingwhale',
+  siteTitleAlt: 'thecodingwhale', // This allows an alternative site title for SEO schema.
+  publisher: 'Aldren Terante', // Organization name used for SEO schema
+  siteDescription: 'Everything related between web development and design.',
+  siteUrl: 'http://thecodingwhale.com/', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
+  postsPerPage: 6, // Number of posts shown on paginated pages
+  author: 'Aldren Terante', // Author for RSS author segment and SEO schema
+  authorUrl: 'https://github.com/thecodingwhale', // URL used for author and publisher schema, can be a social profile or other personal site
+  userTwitter: '@aldrenterante', // Change for Twitter Cards
+  shortTitle: 'thecodingwhale', // Used for App manifest e.g. Mobile Home Screen
+  shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
+  shareImageWidth: 900, // Change to the width of your default share image
+  shareImageHeight: 600, // Change to the height of your default share image
+  siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
+  backgroundColor: '#e9e9e9', // Used for Offline Manifest
+  themeColor: '#121212', // Used for Offline Manifest
+  copyright: 'Copyright © 2019 thecodingwhale', // Copyright string for the RSS feed
+}
